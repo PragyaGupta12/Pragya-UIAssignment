fd920b5a28845f92e18d8c61244a23496577565d
Pragya Gupta <pragya.gupta012@gmail.com>
2024-08-26 12:27:50 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import CustPurchaseSummary from './Components/CustPurchaseSummary';
 import { fetchTransactions } from './services/TransactionData';
 
 function App() {
-  const [transactions, setTransactions] = useState([]);
+  const [transaction, setTransactions] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -35,8 +35,8 @@ function App() {
   return (
     <div className="App">
       <h1>Reward Calculator for Customers</h1>
-      <TransactionDetails transactions={transactions} />
-      <CustPurchaseSummary transactions={transactions} />
+      <TransactionDetails transactions={transaction} />
+      <CustPurchaseSummary transactions={transaction} />
     </div>
   );
 }
